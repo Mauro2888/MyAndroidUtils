e7b24ea3c8223d8813f21ac366322d1c3b48db1a
Mauro Caredda <mauro.dev88@gmail.com>
2017-10-18 11:04:13 +0200

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -12,3 +12,23 @@ public void getBytesFromFile(InputStream input, OutputStream output) throws IOEx
         output.close();
     }
 }
+
+public String SubtractHour(String ora){
+        SimpleDateFormat mFormatOraFromRecycler = new SimpleDateFormat("HH.mm");
+        Date dateRecy = null;
+        Date dateNow = Calendar.getInstance().getTime();
+        try {
+            dateRecy = mFormatOraFromRecycler.parse(ora);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        long subtract = dateRecy.getTime() - dateNow.getTime();
+        Date formatAllMillisToHour = new Date(subtract - 2 * 60 * 60 * 1000);//converto 2 ore * 60 *60 * 1000 (millis )
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(formatAllMillisToHour);
+        int ore = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+    
+        return ore + " " + minute;
+    }
