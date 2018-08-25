643b7c5f74b34084741935121cc66d6295d00dd1
Mauro Caredda <mauro.dev88@gmail.com>
2018-08-26 01:25:55 +0200

    Update MyZipUtils.java

--- MyZipUtils.java
+++ MyZipUtils.java
@@ -24,5 +24,40 @@
         zos.close();
 
     }
+
+/** eseguo
+InputStream inputStream = new FileInputStream("C:\\testZip\\doc.zip");
+        readAndCopy(inputStream);
+*/
+
+public static void readAndCopy(InputStream inputStream) throws IOException {
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("C:\\testZip\\ZML_zipped.zip"));
+        ZipEntry zipEntry = zipInputStream.getNextEntry();
+        writeZipWithXml(zipEntry.getName(), zipOutputStream);
+        zipOutputStream.closeEntry();
+        zipOutputStream.close();
+
+
+    }
+
+public static void writeZipWithXml(String xmlNameFile, ZipOutputStream zipStream)
+           throws IOException {
+
+        File aFile = new File(xmlNameFile);
+        FileInputStream inputStream = new FileInputStream(aFile);
+        ZipEntry zipEntry = new ZipEntry(xmlNameFile);
+        zipStream.putNextEntry(zipEntry);
+
+        byte[] buffer = new byte[inputStream.available()];
+        int length;
+        while ((length = inputStream.read(buffer)) >= 0) {
+            zipStream.write(buffer, 0, length);
+        }
+
+        zipStream.closeEntry();
+        inputStream.close();
+
+}
     
     
