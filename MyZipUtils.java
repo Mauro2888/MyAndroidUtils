cd53d02ee10cb378f73c6257fdd54c115aa44670
Mauro Caredda <mauro.dev88@gmail.com>
2018-08-25 15:36:11 +0200

    Create MyZipUtils.java

---
+++ MyZipUtils.java
@@ -0,0 +1,28 @@
+ 
+ 
+ //copy to new zip content of zip flow
+ public static void copyZipContent() throws IOException {
+
+        ZipFile zipFile = new ZipFile("C:\\testZip\\doc.zip");
+        final ZipOutputStream zos = new ZipOutputStream(new FileOutputStream("C:\\testZip\\diceUp.zip"));
+        for (Enumeration<? extends ZipEntry> e = zipFile.entries(); e.hasMoreElements(); ) {
+            ZipEntry entryIn = e.nextElement();
+            if (entryIn.getName().contains(".xml")) {
+                System.out.println(entryIn.getName());
+                ZipEntry zipEntry = new ZipEntry(entryIn.getName());
+                zos.putNextEntry(zipEntry);
+                InputStream is = zipFile.getInputStream(entryIn);
+                byte[] buf = new byte[is.available()];
+                int len;
+                while ((len = (is.read(buf))) > 0) {
+                    zos.write(buf,0,len);
+                    zos.write(buf);
+                }
+            }
+            zos.closeEntry();
+        }
+        zos.close();
+
+    }
+    
+    
