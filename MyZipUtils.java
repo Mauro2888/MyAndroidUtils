27388db54e710efa015e719af66ac86d0ee22f0e
Mauro Caredda <mauro.dev88@gmail.com>
2018-08-26 01:46:00 +0200

    Update MyZipUtils.java

--- MyZipUtils.java
+++ MyZipUtils.java
@@ -30,32 +30,51 @@ InputStream inputStream = new FileInputStream("C:\\testZip\\doc.zip");
         readAndCopy(inputStream);
 */
 
+ /**
+     *  Decode zip flow from jms with .xml extension
+     *
+     * @param inputStream zip flow from jms
+     * @throws IOException file exception
+     */
+
 public static void readAndCopy(InputStream inputStream) throws IOException {
         ZipInputStream zipInputStream = new ZipInputStream(inputStream);
         ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("C:\\testZip\\ZML_zipped.zip"));
         ZipEntry zipEntry = zipInputStream.getNextEntry();
-        writeZipWithXml(zipEntry.getName(), zipOutputStream);
+       if (zipEntry.getName().contains(".xml")) {
+            writeToZipFile(zipEntry.getName(), zipOutputStream);
+        }else {
+            System.out.println("No Xml file inside zip");
+        }
         zipOutputStream.closeEntry();
         zipOutputStream.close();
 
 
     }
 
-public static void writeZipWithXml(String xmlNameFile, ZipOutputStream zipStream)
+/**
+     * Copy content xml file inside new compressed zip and save it to sftp
+     * 
+     * @param xmlNameFile xml file extracted from jms zip flow
+     * @param zipOutputStream output destination stream 
+     * @throws IOException
+     */
+
+    public static void writeToZipFile(String xmlNameFile, ZipOutputStream zipOutputStream)
            throws IOException {
 
-        File aFile = new File(xmlNameFile);
-        FileInputStream inputStream = new FileInputStream(aFile);
+        File inputXmlFile = new File(xmlNameFile);
+        FileInputStream inputStream = new FileInputStream(inputXmlFile);
         ZipEntry zipEntry = new ZipEntry(xmlNameFile);
-        zipStream.putNextEntry(zipEntry);
+        zipOutputStream.putNextEntry(zipEntry);
 
         byte[] buffer = new byte[inputStream.available()];
         int length;
         while ((length = inputStream.read(buffer)) >= 0) {
-            zipStream.write(buffer, 0, length);
+            zipOutputStream.write(buffer, 0, length);
         }
 
-        zipStream.closeEntry();
+        zipOutputStream.closeEntry();
         inputStream.close();
 
 }
