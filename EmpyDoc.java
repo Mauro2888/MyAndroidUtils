4723dca295540cb7857709e332e347eaae644cf9
Mauro Caredda <mauro.dev88@gmail.com>
2018-08-30 14:28:53 +0200

    Create EmpyDoc.java

---
+++ EmpyDoc.java
@@ -0,0 +1,21 @@
+ private Processor checkBlankFile = new Processor() {
+
+              @Override
+              public void process(Exchange exchange) throws Exception {
+                     boolean amountFlag = true;
+                     String strMessage = exchange.getIn().getBody(String.class);
+                     DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+                     InputSource src = new InputSource();
+                     src.setCharacterStream(new StringReader(strMessage));	
+                     Document doc = builder.parse(src);	
+                     String XPATH_EXPRESSION = "//*[local-name()='businessTransaction']/path/path";
+                     XPath xPath = XPathFactory.newInstance().newXPath();
+                     if(!strMessage.contains("lineItem"))	
+                     {	
+                            String settleAmount = xPath.evaluate(XPATH_EXPRESSION,doc).toString();       	
+                            amountFlag = Float.parseFloat(settleAmount)!=0;	
+                     }	
+                  
+                     exchange.getIn().setHeader("flagValue", String.valueOf(amountFlag));	
+              }	
+       };
