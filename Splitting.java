d5f9c05be0edf26dbd522e3f6fa1629bf4970a3a
Mauro Caredda <mauro.dev88@gmail.com>
2018-09-17 12:20:27 +0200

    Create Splitting.java

---
+++ Splitting.java
@@ -0,0 +1,36 @@
+//if files are overwrited try 
+// to("file:xxx?fileExist=Append"); 
+// example - from(src).split().method(splitbean which returns an custom Iterator).streaming().to(file:...?fileExist=Append); 
+
+ List<Message> messages = new ArrayList<Message>(); 
+        String line = null; 
+        int count = 0; 
+        int fileNameCount = 0; 
+        StringBuffer sb = new StringBuffer(); 
+        try { 
+            while (null != (line = inputReader.readLine())) { 
+                sb.append(line); 
+                count++; 
+
+                if (count == 5) { 
+                    messages.add(createNewOutput(sb, "Sample"+fileNameCount+".xml")); 
+                    count = 0; 
+                    sb = new StringBuffer(); 
+                    fileNameCount++; 
+                } 
+            } 
+
+            inputReader.close(); 
+        } catch(Exception ex) { 
+            ex.printStackTrace(); 
+        } 
+
+        return messages.iterator(); 
+    } 
+
+    private Message createNewOutput(StringBuffer sb, String fileName) throws EOFException{ 
+        Message message = new DefaultMessage(); 
+        message.setBody(sb.toString()); 
+        message.setHeader(Exchange.FILE_NAME, fileName); 
+        return message; 
+    } 
