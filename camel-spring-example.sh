027bc8bb3cdb55991891a4fddbb051f9e4d76a71
Mauro Caredda <mauro.dev88@gmail.com>
2018-10-29 10:26:44 +0100

    Add files via upload

---
+++ camel-spring-example.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate                            \
+  -DarchetypeGroupId=org.apache.camel.archetypes  \
+  -DarchetypeArtifactId=camel-archetype-spring    \
+  -DarchetypeVersion=2.10.3                       \
+  -DgroupId=com.nexse.swat.example                \
+  -DartifactId=camel-spring                       \
+  -DinteractiveMode=false   
