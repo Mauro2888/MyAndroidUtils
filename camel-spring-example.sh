8c652577fc0714c45f4aa74321f72dac0e1e45e7
Mauro Caredda <mauro.dev88@gmail.com>
2018-10-29 10:29:28 +0100

    Update camel-spring-example.sh

--- camel-spring-example.sh
+++ camel-spring-example.sh
@@ -2,6 +2,6 @@ mvn archetype:generate                            \
   -DarchetypeGroupId=org.apache.camel.archetypes  \
   -DarchetypeArtifactId=camel-archetype-spring    \
   -DarchetypeVersion=2.10.3                       \
-  -DgroupId=com.nexse.swat.example                \
+  -DgroupId=com.test.spring.integration              \
   -DartifactId=camel-spring                       \
   -DinteractiveMode=false   
