a4ecd439ffff2b409771c46a169cf61ca59b0823
Mauro Caredda <mauro.dev88@gmail.com>
2018-11-25 18:04:14 +0100

    Update CopyFilesWithoutCamel.java

--- CopyFilesWithoutCamel.java
+++ CopyFilesWithoutCamel.java
@@ -1,22 +1,14 @@
 public class CopyFilesJava {
 
     public static void main(String[] args) throws IOException {
-    //noop=true options for copy
-    
-    
-    /**Classe copia file con e senza route camel
-        from(file:/data/input?noop=true)
-        .to(file:/data/output);
-         **/
-    
-    
-        File input = new File("/data/input");
-        File output = new File("/data/output");
-        
+
+        File input = new File("data/input");
+        File output = new File("data/output");
+
         File[] files = input.listFiles();
         for (File source :files) {
             if (source.isFile()){
-                FileOutputStream fileOutputStream = new FileOutputStream(output.getPath() + source.getName());
+                FileOutputStream fileOutputStream = new FileOutputStream(output.getPath() + File.separator + source.getName());
                 FileInputStream fileInputStream = new FileInputStream(source);
                 byte[] buffer = new byte[(int) source.length()];
                 fileInputStream.read(buffer);
