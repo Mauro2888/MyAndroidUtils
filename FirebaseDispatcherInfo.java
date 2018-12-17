7ed5c53b7f20c8df2d488219ec41e189940f53b8
Mauro Caredda <mauro.dev88@gmail.com>
2018-12-17 12:45:47 +0100

    Update FirebaseDispatcherInfo.java

--- FirebaseDispatcherInfo.java
+++ FirebaseDispatcherInfo.java
@@ -16,3 +16,41 @@ Job myJob = dispatcher.newJobBuilder()
     .setService(MyJobService.class) // the JobService that will be called
     .setTag("my-unique-tag")        // uniquely identifies the job
     .build();
+
+//OR 
+
+//Scheduling a complext  job
+Bundle myExtrasBundle = new Bundle();
+myExtrasBundle.putString("some_key", "some_value");
+
+Job myJob = dispatcher.newJobBuilder()
+    // the JobService that will be called
+    .setService(MyJobService.class)
+    // uniquely identifies the job
+    .setTag("my-unique-tag")
+    // one-off job
+    .setRecurring(false)
+    // don't persist past a device reboot
+    .setLifetime(Lifetime.UNTIL_NEXT_BOOT)
+    // start between 0 and 60 seconds from now
+    .setTrigger(Trigger.executionWindow(0, 60))
+    // don't overwrite an existing job with the same tag
+    .setReplaceCurrent(false)
+    // retry with exponential backoff
+    .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+    // constraints that need to be satisfied for the job to run
+    .setConstraints(
+        // only run on an unmetered network
+        Constraint.ON_UNMETERED_NETWORK,
+        // only run when the device is charging
+        Constraint.DEVICE_CHARGING
+    )
+    .setExtras(myExtrasBundle)
+    .build();
+
+dispatcher.mustSchedule(myJob);
+
+//Cancelling a job
+dispatcher.cancel("my-unique-tag");
+//Cancelling all jobs
+dispatcher.cancelAll();
