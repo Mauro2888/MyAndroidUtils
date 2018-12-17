672bf78496964797ecfccde49a56eb53bf806908
Mauro Caredda <mauro.dev88@gmail.com>
2018-12-17 12:43:36 +0100

    Update and rename FirebaseDispatcherInfo.txt to FirebaseDispatcherInfo.java

---
+++ FirebaseDispatcherInfo.java
@@ -0,0 +1,17 @@
+- declare class for dispatcher that extends com.firebase.jobdispatcher.JobService
+
+<service android:name=".SeviceBg" android:exported="false">
+    <intent-filter>
+        <action android:name="com.firebase.jobdispatcher.ACTION_EXECUTE"/>
+    </intent-filter>
+</service>
+
+- declare new instance for firebasedispatcher 
+ //initialize dispatcher
+        FirebaseJobDispatcher jobDispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+        
+Scheduling a simple job
+Job myJob = dispatcher.newJobBuilder()
+    .setService(MyJobService.class) // the JobService that will be called
+    .setTag("my-unique-tag")        // uniquely identifies the job
+    .build();
